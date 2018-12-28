59f3d52af83a9c58ad0240bc3b18c3f17dae6541
Lucas Harvey <l4harvey@uwaterloo.ca>
2018-12-27 21:27:49 -0500

    Updates

---
+++ precache-manifest.558db460597b5cbc3783af64107a7a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "099eca548b7ff06f78a9d03d91c9c2c9",
+    "url": "/static/media/water2.099eca54.png"
+  },
+  {
+    "revision": "8ba744f6fd7b1919a2ab395bbac82830",
+    "url": "/static/media/round-me.8ba744f6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34f541ba902fc700250c",
+    "url": "/static/js/main.34f541ba.chunk.js"
+  },
+  {
+    "revision": "bea3696eca004b1190be",
+    "url": "/static/js/1.bea3696e.chunk.js"
+  },
+  {
+    "revision": "34f541ba902fc700250c",
+    "url": "/static/css/main.073a059d.chunk.css"
+  },
+  {
+    "revision": "8903261fbf016f6359e298cb02b5211e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.61ba4915391e88fca5002b41959aeafa.js"
+  "/precache-manifest.558db460597b5cbc3783af64107a7a5b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/personal-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
